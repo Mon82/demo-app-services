9140805f6c71dbd82e2d2803543299c01f8f1d4a
Carlos Montero <csmp@gft.com>
2021-07-16 15:50:07 +0200

    push action

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+// commit para ejecutar el push
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
